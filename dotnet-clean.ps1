a2d0a2fd4813a152302907b764e07ebfcfc3a024
Mohit Goyal <jgoyalmo@jci.com>
2018-08-29 02:35:29 +0530

    Added PowerShell script to clean publish directory

---
+++ dotnet-clean.ps1
@@ -0,0 +1,21 @@
+﻿[CmdletBinding()]
+Param (
+    [parameter(Mandatory=$true)]
+    [String] $directory,
+
+    [parameter(Mandatory=$true)]
+    [String] $workingdirectory
+)
+
+if(Test-Path $workingdirectory){
+   Set-Location $workingdirectory
+}
+else{
+   Write-Host "Provided working directory does not exists"
+   Write-Host "Proceeding in the current directory: $pwd"
+}
+
+foreach($Item in (Get-ChildItem -Recurse -Directory | Where {$_.Name -eq $directory})){
+   Write-Host "Removing $($item.FullName)"
+   Remove-Item $($Item.FullName) -Force -Recurse -ErrorAction SilentlyContinue
+}
