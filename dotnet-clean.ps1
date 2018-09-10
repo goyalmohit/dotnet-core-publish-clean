dc74a4e78b665b4c932e078259633fb90780a6ca
Mohit Goyal <goyalmohit@users.noreply.github.com>
2018-09-11 04:22:35 +0530

    Added some comments to help understand PS script

--- dotnet-clean.ps1
+++ dotnet-clean.ps1
@@ -1,12 +1,18 @@
-﻿[CmdletBinding()]
+﻿# This script clears dotnet core publish directory
+[CmdletBinding()]
 Param (
+    # Defines the publish directory name
     [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
     [String] $directory,
 
+    # Defines the full path for working directory context
     [parameter(Mandatory=$true)]
+    [ValidateNotNullOrEmpty()]
     [String] $workingdirectory
 )
 
+# Determines the context of working directory
 if(Test-Path $workingdirectory){
    Set-Location $workingdirectory
 }
@@ -15,7 +21,10 @@ else{
    Write-Host "Proceeding in the current directory: $pwd"
 }
 
+# Clears all files present in the publish directory
 foreach($Item in (Get-ChildItem -Recurse -Directory | Where {$_.Name -eq $directory})){
    Write-Host "Removing $($item.FullName)"
    Remove-Item $($Item.FullName) -Force -Recurse -ErrorAction SilentlyContinue
 }
+
+Write-Host "End of script."
